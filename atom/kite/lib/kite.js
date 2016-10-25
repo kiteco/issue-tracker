3b052dfbec84d7060bdd1cc5cd7c5c8ab8ce8cfe
Alex Flint <alex.flint@gmail.com>
2016-10-24 20:16:59 -0700

    factor out completions into separate module

--- atom/kite/lib/kite.js
+++ atom/kite/lib/kite.js
@@ -7,6 +7,7 @@ var fs = require('fs');
 var child_process = require('child_process');
 var process = require('process');
 var http = require('http');
+var utils = require('./utils.js')
 
 var DEBUG = false;
 
@@ -145,7 +146,7 @@ var KiteOutgoing = {
   buildEvent: function(editor, action) {
     var text = editor.getText();
     var cursorPoint = editor.getCursorBufferPosition();
-    var cursorOffset = this.pointToOffset(text, cursorPoint);
+    var cursorOffset = utils.pointToOffset(text, cursorPoint);
 
     // don't send content over 1mb
     if (text.length > (1 << 20)) {
@@ -164,17 +165,6 @@ var KiteOutgoing = {
         "end": cursorOffset,
       }],
     };
-  },
-  // pointToOffet takes the contents of the buffer and a point object
-  // representing the cursor, and returns a byte-offset for the cursor
-  pointToOffset: function(text, point) {
-    var lines = text.split("\n");
-    var total = 0;
-    for (var i = 0; i < lines.length && i < point.row; i++) {
-      total += lines[i].length;
-    }
-    total += point.column + point.row; // we add point.row to add in all newline characters
-    return total;
   },
 };
 
@@ -335,91 +325,7 @@ var KiteIncoming = {
   },
 };
 
-var CompletionsProvider = {
-  selector: '.source.python',
-  disableForSelector: '.source.python .comment, .source.python .string',
-  inclusionPriority: 2,
-  suggestionPriority: 2,
-  excludeLowerPriority: false,
-
-  // called to handle diff suggestions
-  getSuggestions: function(params) {
-    return new Promise(function (resolve, reject) {
-      var text = params.editor.getText();
-      var cursor = KiteOutgoing.pointToOffset(text, params.bufferPosition);
-      var payload = {
-        "filename": params.editor.getPath(),
-        "text": text,
-        "cursor": cursor,
-      };
-
-      // don't send content over 1mb
-      if (payload.text.length > (1 << 20)) {
-        console.log("buffer contents too large, not attempting completions");
-        reject();
-        return;
-      }
-
-      var callback = function(response) {
-        var str = '';
-        response.on('data', function (chunk) {
-          str += chunk;
-        });
-
-        response.on('end', function () {
-          if (response.statusCode == 404) {
-            // This means we had no completions for this cursor position. Do not call
-            // reject() because that will generate an error in the console.
-            resolve([]);
-            return;
-          } else if (response.statusCode != 200) {
-            reject("error from kited: " + str);
-            return;
-          }
-
-          try {
-            var resp = JSON.parse(str);
-          } catch (ex) {
-            reject("error parsing response from kited: " + ex);
-            return;
-          }
-
-          try {
-            var suggestions = [];
-            for (var i = 0; i < resp.completions.length; i++) {
-              c = resp.completions[i];
-              suggestions.push({
-                text: c.display,
-                type: c.hint,
-                rightLabel: c.hint,
-              });
-            }
-            resolve(suggestions);
-          } catch (ex) {
-            reject("error processing completions from kited: " + ex);
-            return;
-          }
-        });
-      };
-
-      var options = {
-        host: '127.0.0.1',
-        port: '46624',
-        path: '/clientapi/editor/completions',
-        method: 'POST'
-      };
-
-      var req = http.request(options, callback);
-      req.write(JSON.stringify(payload));
-      req.end();
-    });
-  },
-
-  // called when one of our suggestion is inserted by the user
-  onDidInsertSuggestion: function(params) {
-    // TODO: notify kite that a suggestion was accepted
-  },
-};
+var CompletionsProvider = require('./completions')
 
 module.exports = {
   outgoing: KiteOutgoing,
@@ -435,6 +341,6 @@ module.exports = {
     atom.workspace.onDidChangeActivePaneItem(this.outgoing.onFocus.bind(this.outgoing));
   },
   completions: function() {
-    return CompletionsProvider;
+    return require('./completions.js');
   }
 };
